14b40c50cc2de331bfd8986cc6cfe23fc7140f3e
MeteorTheLizard <55365852+MeteorTheLizard@users.noreply.github.com>
2019-09-29 23:21:27 +0200

    Initial Githubification
    Published to Github

---
+++ lua/autorun/server/sv_tinycppi.lua
@@ -0,0 +1,41 @@
+--Made by MrRangerLP
+
+if SERVER then
+	local meta = FindMetaTable("Entity")
+	if not meta.CPPIGetOwner then
+		CPPIGetOwner = function(Ent)
+			local Owner = Ent:GetNWEntity("my_owner")
+			if Owner then
+				return Owner
+			end
+		end
+	end
+
+	hook.Add("PlayerSpawnedEffect","TinyCPPI_Effect",function(ply,_,ent)
+		ent:SetNWEntity("my_owner",ply)
+	end)
+
+	hook.Add("PlayerSpawnedNPC","TinyCPPI_NPC",function(ply,ent)
+		ent:SetNWEntity("my_owner",ply)
+	end)
+
+	hook.Add("PlayerSpawnedProp","TinyCPPI_Prop",function(ply,_,ent)
+		ent:SetNWEntity("my_owner",ply)
+	end)
+
+	hook.Add("PlayerSpawnedRagdoll","TinyCPPI_Ragdoll",function(ply,model,ent)
+		ent:SetNWEntity("my_owner",ply)
+	end)
+
+	hook.Add("PlayerSpawnedSENT","TinyCPPI_SENT",function(ply,ent)
+		ent:SetNWEntity("my_owner",ply)
+	end)
+
+	hook.Add("PlayerSpawnedSWEP","TinyCPPI_SWEP",function(ply,ent)
+		ent:SetNWEntity("my_owner",ply)
+	end)
+
+	hook.Add("PlayerSpawnedVehicle","TinyCPPI_Vehicle",function(ply,ent)
+		ent:SetNWEntity("my_owner",ply)
+	end)
+end

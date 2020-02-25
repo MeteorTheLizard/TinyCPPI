8ad6ec87eb0563388ebb72ebb86f283de67f01dd
Meteor <55365852+MeteorTheLizard@users.noreply.github.com>
2020-02-25 16:56:01 +0100

    Update cl_init.lua

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -1,13 +1,15 @@
 AddCSLuaFile()
 
 if CLIENT then
-  local meta = FindMetaTable("Entity")
+  	local meta = FindMetaTable("Entity")
 	if not meta.CPPIGetOwner then
 		function meta:CPPIGetOwner()
 			local Owner = self:GetNWEntity("my_owner")
 			if Owner then
 				return Owner
 			end
+
+			return
 		end
 	end
  end

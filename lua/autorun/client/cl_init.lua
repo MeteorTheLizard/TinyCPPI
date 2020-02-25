a4e842d26a1257849075f23d334ffb441a179082
Meteor <55365852+MeteorTheLizard@users.noreply.github.com>
2020-02-25 16:55:21 +0100

    Create cl_init.lua

---
+++ lua/autorun/client/cl_init.lua
@@ -0,0 +1,13 @@
+AddCSLuaFile()
+
+if CLIENT then
+  local meta = FindMetaTable("Entity")
+	if not meta.CPPIGetOwner then
+		function meta:CPPIGetOwner()
+			local Owner = self:GetNWEntity("my_owner")
+			if Owner then
+				return Owner
+			end
+		end
+	end
+ end
